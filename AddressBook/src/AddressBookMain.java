e13a70686bcf1bce11c89f3ff7e43bfdd0cbcb2c
Darshan Patil <iampatildarsh>
2023-01-07 16:47:42 +0530

    Sorting address Book contact entries by Person's name

--- AddressBook/src/AddressBookMain.java
+++ AddressBook/src/AddressBookMain.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class AddressBookMain {
 
@@ -13,13 +14,14 @@ class AddressBookMain {
         while (flag) {
             System.out.println("1.Add New Address Book");
             System.out.println("2.Find Duplicate Entry in Address Book");
-            System.out.println("3.Search Contact from a city");
+            System.out.println("3.Search Contact from a City");
             System.out.println("4.Search Contact from a State");
             System.out.println("5.View contact By State Using State and Person HashMap");
             System.out.println("6.View Contact by city Using City and Person HashMap");
             System.out.println("7.Count Contact By State");
             System.out.println("8.Count Contact By City");
-            System.out.println("9.Exit");
+            System.out.println("9.Sort the Contacts in Alphabetically Order");
+            System.out.println("10.Exit");
 
             System.out.println("Enter choice: ");
             int option = sc.nextInt();
@@ -78,6 +80,11 @@ class AddressBookMain {
                     break;
 
                 case 9:
+                    System.out.println("Sort Contact");
+                    addressBookMain.sortContactByName();
+                    break;
+
+                case 10:
                     flag = false;
                     break;
             }
@@ -92,7 +99,7 @@ class AddressBookMain {
 
             System.out.println("1.Add Contact");
             System.out.println("2.Edit Contact");
-            System.out.println("3.Delete");
+            System.out.println("3.Delete Contact");
             System.out.println("4.Exit");
             System.out.println("Enter Choice: ");
 
@@ -181,16 +188,15 @@ class AddressBookMain {
 
     // UC10- method to count no.of persons by state name
     public void CountByState(String state) {
-        int count = 0;
+        int countPersonInState = 0;
         for (Map.Entry<String, AddressBook> entry : addressBookListMap.entrySet()) {
             for (int i = 0; i < (entry.getValue()).contactList.size(); i++) {
                 Contacts contact = entry.getValue().contactList.get(i);
-                if (state.equals(contact.getState())) {
-                    count++;
-                }
+                if (state.equals(contact.getState()))
+                    countPersonInState++;
             }
         }
-        System.out.println("Total Person Count in state " + state + ": " + count);
+        System.out.println("Total Count of Persons in state " + state + ": " + countPersonInState);
     }
 
     // UC10- method to count no.of persons by city name
@@ -199,11 +205,23 @@ class AddressBookMain {
         for (Map.Entry<String, AddressBook> entry : addressBookListMap.entrySet()) {
             for (int i = 0; i < (entry.getValue()).contactList.size(); i++) {
                 Contacts d = (Contacts) entry.getValue().contactList.get(i);
-                if (city.equals(d.getCity())) {
+                if (city.equals(d.getCity()))
                     countPersonInCity++;
-                }
             }
         }
-        System.out.println("Total number of people in this city " + city + ": " + countPersonInCity);
+        System.out.println("Total Count of Persons in City " + city + ": " + countPersonInCity);
+    }
+
+    // UC11- Sorting the Contacts by Name
+    private void sortContactByName() {
+        for (Map.Entry<String, AddressBook> entry : addressBookListMap.entrySet()) {
+            AddressBook value = entry.getValue();
+            List<Contacts> sortedList = value.contactList.stream().sorted(Comparator.comparing(Contacts::getFirstName)).collect(Collectors.toList());
+
+            for (Contacts contact : sortedList) {
+                System.out.println("First Name: " + contact.getFirstName());
+                System.out.println("Last Name: " + contact.getLastname());
+            }
+        }
     }
 }
